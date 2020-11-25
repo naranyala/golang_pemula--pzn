0591afffe47c671d60331a7e1ba4e1b4b923f33a
naranyala <fudzer.mh@gmail.com>
2020-11-25 22:18:51 +0700

    (+) menambahkan file 09_increment.go - increment dan decrement

---
+++ 09_increment.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	fmt.Println(i)
+
+	//increment
+	//i = i + 1
+	//or
+	i++
+
+	fmt.Println(i)
+	//decrement
+	i--
+	fmt.Println(i)
+	i--
+	fmt.Println(i)
+
+}
