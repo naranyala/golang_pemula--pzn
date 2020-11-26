cfe481ec67fb80435034ce256baabfdcea4ff64b
naranyala <fudzer.mh@gmail.com>
2020-11-26 11:27:37 +0700

    (+) 15_array.go

---
+++ 15_array.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [5]string
+
+	names[0] = "Fudzer"
+	names[1] = "Huda"
+	names[2] = "Rizal"
+	fmt.Println(names)
+	fmt.Println(names[0] + " " + names[1])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+	fmt.Println("---")
+
+	//for i := 0; i < 5; i++ {
+	for i := 0; i < len(names); i++ {
+		fmt.Print(i, " ")
+		fmt.Println(names[i])
+	}
+	fmt.Println("---")
+	//for index, value := range names {
+	for _, value := range names {
+		//fmt.Println("index", index, "=", value)
+		fmt.Println(value)
+	}
+}
