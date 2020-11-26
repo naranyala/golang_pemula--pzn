70f2529a03be1ba6534cd4ba7da8f35d0c05a3f4
naranyala <fudzer.mh@gmail.com>
2020-11-26 07:27:27 +0700

    (+) menambahkan file 10_comparator.go

---
+++ 10_comparator.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 2
+	b := 2
+
+	//result := a == b  //sama dengan
+	//result2 := a != b //tidak sama dengan
+	//result3 := a < b //kurang dari
+	//result4 := a > b //lebih dari
+	//result5 := a <= b //kurang dar atau sama dengan
+	result6 := a >= b //kurang dar atau sama dengan
+
+	//fmt.Println(result)
+	//fmt.Println(result2)
+	//fmt.Println(result3)
+	//fmt.Println(result4)
+	//fmt.Println(result5)
+	fmt.Println(result6)
+}
