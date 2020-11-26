17616dc4dc1712d44202f67e6a67ec25a26e3b3e
naranyala <fudzer.mh@gmail.com>
2020-11-26 07:43:29 +0700

    (+) menambahkan file 11_for.go

---
+++ 11_for.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//i := 1
+	//for i < 10 {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i, "Hello World")
+		//i = i + 1
+		//i++
+	}
+
+	fmt.Println("Selesai")
+
+	//for i := 10; i >= 1; i-- {
+	//	fmt.Println(i, "Hello World")
+	//}
+}
